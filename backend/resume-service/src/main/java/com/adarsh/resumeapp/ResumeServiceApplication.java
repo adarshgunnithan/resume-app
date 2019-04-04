5f30566c05a165232e7b561f0e56b25db60aafb6
adarsh <adarshgunnithan2017@gmail.com>
2019-04-04 18:38:55 +0530

    changes for swagger

--- backend/resume-service/src/main/java/com/adarsh/resumeapp/ResumeServiceApplication.java
+++ backend/resume-service/src/main/java/com/adarsh/resumeapp/ResumeServiceApplication.java
@@ -2,14 +2,6 @@ package com.adarsh.resumeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * @author 660186
@@ -26,21 +18,5 @@ public class ResumeServiceApplication {
 		SpringApplication.run(ResumeServiceApplication.class, args);
 	}
 	
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.adarsh.resumeapp.controller")).build();
-	}
-
-	/**
-	 * Custom meta data
-	 * @return ApiInfo
-	 */
-	private ApiInfo metaData() {
-		ApiInfo apiInfo = new ApiInfo("Spring Boot REST API", "Spring Boot REST API for Resume Application", "1.0",
-				"Terms of service", new Contact("Adarsh G Unnithan", "9744835312", "adarshgunnithan2017@gmail.com"),
-				"Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
-		return apiInfo;
-	}
 
 }
---
+++ backend/resume-service/src/main/java/com/adarsh/resumeapp/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.adarsh.resumeapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;*/
+
+/**
+ * @author Adarsh
+ *  SwaggerConfig - for enabling swagger documentation
+ */
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+	
+/*	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().build();
+					}*/
+
+/*	*//**
+	 * Custom meta data
+	 * @return ApiInfo
+	 *//*
+	private ApiInfo metaData() {
+		ApiInfo apiInfo = new ApiInfo("Spring Boot REST API", "Spring Boot REST API for Resume Application", "1.0",
+				"Terms of service", new Contact("Adarsh G Unnithan", "9744835312", "adarshgunnithan2017@gmail.com"),
+				"Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+		return apiInfo;
+	}*/
+
+}
