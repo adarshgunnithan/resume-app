774579b975371c5cff2d230b7f7c704411faebd9
adarsh <adarshgunnithan2017@gmail.com>
2019-04-04 13:58:07 +0530

    backend code added

---
+++ backend/resume-service/src/main/java/com/adarsh/resumeapp/controller/ResumeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adarsh.resumeapp.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeServiceApplication.class, args);
+	}
+
+}
---
+++ backend/resume-service/src/test/java/com/adarsh/resumeapp/controller/test/ResumeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adarsh.resumeapp.controller.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResumeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
