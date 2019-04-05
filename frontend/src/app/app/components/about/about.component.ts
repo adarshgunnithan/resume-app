c4baa5d1b1c5e3dd0742d9eb47d901402beace31
Adarsh G Unnithan <Adarsh.GUnnithan@cognizant.com>
2019-04-05 22:02:28 +0530

    fixed issue due variable scope

--- frontend/src/app/app/components/about/about.component.ts
+++ frontend/src/app/app/components/about/about.component.ts
@@ -10,32 +10,42 @@ import {About} from '../../../about'
 export class AboutComponent implements OnInit {
   aboutContent : Array<About>;
   private address: string;
-  // phoneNumber: string;
-  // descText: string;
-  // email: string;
+  private phoneNumber: string;
+  private descText: string;
+  private email: string;
 
   constructor(private resumeSer: ResumeService) {
     this.aboutContent=[];
    this.address="";
+   this.phoneNumber="";
+   this.descText="";
+   this.email="";
    }
 
   ngOnInit() {
     this.resumeSer.getAboutPageContent().subscribe((aboutData)=>{
       
     this.aboutContent.push(...aboutData);  
-     this.aboutContent.forEach(function (value) {
+     this.aboutContent.forEach(value => { 
           if(value.aboutKeyName === "ADDRESS"){
-    
-         // this.address=value.aboutValueText;
-        //  console.log( this.address);
-        
+         this.address=value.aboutValueText;
+          }else if(value.aboutKeyName === "PHONE_NUMBER"){
+            this.phoneNumber=value.aboutValueText;
+          }else if(value.aboutKeyName === "EMAIL"){
+            this.email=value.aboutValueText;
+          }else if(value.aboutKeyName === "ABOUT_TXT"){
+            this.descText===value.aboutValueText;
+          }else{
+
           }
+
+
       });
   
     
 
 }); 
-
+console.log("address2", this.address);
 
   }
 
